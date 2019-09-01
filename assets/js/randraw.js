9923d3bd1dda1de7cb127f1905fd1f29021c909d
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2019-09-01 18:25:54 +0300

    Fixed how output is stored and processed. Scoring is fixed. Page resize is fixed.

--- assets/js/randraw.js
+++ assets/js/randraw.js
@@ -8,27 +8,9 @@ function scaleImageSize(iw, ih, cw) {
   return [cw, newH, cw, newH, ih / iw];
 }
 
-function reDrawImages() {
-  let img = window.RandrawState.ImgIn;
-  let canvasIn = window.RandrawState.Canvas.In;
-  let canvasOut = window.RandrawState.Canvas.Out;
-  let [imgW, imgH] = [img.width, img.height];
-  let [newImgW, newImgH, newCanW, newCanH, imgRatio] = scaleImageSize(imgW, imgH, canvasIn.offsetWidth);
-  window.RandrawState['imgRatio'] = imgRatio;
-  canvasIn.width = newCanW;
-  canvasIn.height = newCanH;
-  canvasOut.width = newCanW;
-  canvasOut.height = newCanH;
-  let contextIn = window.RandrawState.CanvasContext.In;
-  contextIn.drawImage(img, 0, 0, imgW, imgH, 0, 0, newImgW, newImgH);
-  let contextOut = window.RandrawState.CanvasContext.Out;
-  contextOut.fillStyle = '#eeeeee';
-  contextOut.fillRect(0, 0, newCanW, newCanH);
-}
-
 function scoreImgData(data1, data2) {
   if (!data1 || !data2 || (data1.length !== data2.length)) {
-    return 0;
+    throw new Error('Invalid scoring img data');
   }
   let sum = 0;
   for (let i = 0; i < data1.length; i++) {
@@ -38,33 +20,88 @@ function scoreImgData(data1, data2) {
   return sum;
 }
 
+function scoreImgDataFromWhite(data) {
+  if (!data) {
+    throw new Error('Invalid scoring img data');
+  }
+  let sum = 0;
+  for (let i = 0; i < data.length; i++) {
+    let [a,b] = [data[i], 255];
+    sum += Math.max(a,b) - Math.min(a,b);
+  }
+  return sum;
+}
+
+function drawOutputToDisplay() {
+  let state = window.RandrawState;
+  let contextOut = state.CanvasContext.Out;
+  let outputCanvas = state.Output.MainCanvas;
+  let [inpW, inpH] = state.Input.size();
+  let [newW, newH] = state.ScaledImgSize;
+  contextOut.drawImage(outputCanvas, 0, 0, inpW, inpH, 0, 0, newW, newH);
+}
+
+function reDrawImages() {
+  let state = window.RandrawState;
+  let img = state.Input.MainImg;
+  let canvasIn = state.Canvas.In;
+  let canvasOut = state.Canvas.Out;
+  let [imgW, imgH] = [img.width, img.height];
+  let [newImgW, newImgH, newCanW, newCanH, imgRatio] = scaleImageSize(imgW, imgH, canvasIn.offsetWidth);
+  state['imgRatio'] = imgRatio;
+  state.ScaledImgSize = [newImgW, newImgH];
+  canvasIn.width = newCanW;
+  canvasIn.height = newCanH;
+  canvasOut.width = newCanW;
+  canvasOut.height = newCanH;
+  let contextIn = state.CanvasContext.In;
+  let contextOut = state.CanvasContext.Out;
+  contextIn.drawImage(img, 0, 0, imgW, imgH, 0, 0, newImgW, newImgH);
+  drawOutputToDisplay();
+}
+
 function updateUi() {
   let counter = window.RandrawState.Counter;
-  $('#iteration-counter').text(`${counter.Iteration.Successful} / ${counter.Iteration.Total}`);
   $('#score-counter').text(counter.Score);
+  $('#iteration-counter').text(`${counter.Iteration.Successful} / ${counter.Iteration.Total}`);
+  $('#time-counter').text(Utils.millisToTimeStr(counter.Time.dur()));
+  drawOutputToDisplay();
 }
 
 function run() {
 
-  window.RandrawState.Counter.Iteration.totalUp();
+  let state = window.RandrawState;
+
+  let time = Utils.currentMillis();
+  if (state.PauseRequest.flag) {
+    state.PauseRequest.flag = false;
+    state.Counter.Time.PauseStart = time;
+    updateUi();
+    return;
+  }
+  if (state.Counter.Time.TimerStart < 0) {
+    state.Counter.Time.TimerStart = time;
+  } else if (state.Counter.Time.PauseStart > 0) {
+    let pauseLen = time - state.Counter.Time.PauseStart;
+    state.Counter.Time.PauseStart = -1;
+    state.Counter.Time.TimerStart += pauseLen;
+  }
+
+  state.Counter.Iteration.totalUp();
 
   // Output Size
-  let canvasOut = window.RandrawState.Canvas.Out;
-  let [canW, canH] = [canvasOut.offsetWidth, canvasOut.offsetHeight];
+  let canvasOut = state.Output.MainCanvas;
+  let [canW, canH] = [canvasOut.width, canvasOut.height];
 
   // Input Data
-  let contextIn = window.RandrawState.CanvasContext.In;
-  let dataIn = contextIn.getImageData(0,0,canW,canH).data;
+  let dataIn = state.Input.MainImgData;
 
   // Score output data before
-  let contextOut = window.RandrawState.CanvasContext.Out;
+  let contextOut = state.Output.MainCanvasContext;
   let imageDataOut1 = contextOut.getImageData(0,0,canW,canH);
   let dataOut1 = imageDataOut1.data;
   let score1 = scoreImgData(dataIn, dataOut1);
-  if (window.RandrawState.Const.InitScore <= 0) {
-    window.RandrawState.Const.InitScore = score1;
-  }
-  let scoreRatio = score1 / window.RandrawState.Const.InitScore;
+  let scoreRatio = score1 / state.Input.InitScore;
 
   // Random rect
   let [x, y] = [Utils.randomInt(5, canW - 5), Utils.randomInt(5, canH - 5)];
@@ -86,8 +123,8 @@ function run() {
   if (score1 < score2) {
     contextOut.putImageData(imageDataOut1, 0, 0);
   } else {
-    window.RandrawState.Counter.Score = scoreRatio;
-    window.RandrawState.Counter.Iteration.successfulUp();
+    state.Counter.Score = scoreRatio;
+    state.Counter.Iteration.successfulUp();
   }
   setTimeout(function () {
     updateUi();
@@ -95,18 +132,43 @@ function run() {
   }, 0);
 }
 
-function loadImage() {
+function newWhiteCanvas(w, h) {
+  let c = $("<canvas>")
+    .attr("width", w)
+    .attr("height", h)[0];
+  let ctx = c.getContext("2d");
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, w, h);
+  return c;
+
+}
+
+function getImgData(img) {
+  let c = newWhiteCanvas(img.width, img.height);
+  let ctx = c.getContext('2d');
+  ctx.drawImage(img, 0, 0, img.width, img.height);
+  return ctx.getImageData(0, 0, img.width, img.height).data;
+}
+
+function loadImage(cb) {
   let img = new Image();
   img.onload = () => {
-    window.RandrawState['ImgIn'] = img;
+    let state = window.RandrawState;
+    state.Input.MainImg = img;
+    state.Input.MainImgData = getImgData(img);
+    state.Output.MainCanvas = newWhiteCanvas(img.width, img.height);
+    state.Output.MainCanvasContext = state.Output.MainCanvas.getContext('2d');
+    state.Input.InitScore = scoreImgDataFromWhite(state.Input.MainImgData);
     reDrawImages();
+    cb();
   };
   img.src = './tile.png';
 }
 
 function init() {
-  loadImage();
-  run();
+  loadImage(() => {
+    run();
+  });
 }
 
 function loadHandlebarsPartials(filemap = {}, callback) {
@@ -141,6 +203,18 @@ $(function() {
     let canvasIn = document.getElementById('canvas-in');
     let canvasOut = document.getElementById('canvas-out');
     window['RandrawState'] = {
+        Input: {
+          MainImg: null,
+          MainImgData: null,
+          size: function () {
+            return [this.MainImg.width, this.MainImg.height];
+          },
+          InitScore: -1,
+        },
+        Output: {
+          MainCanvas: null,
+          MainCanvasContext: null,
+        },
         Canvas: {
           In: canvasIn,
           Out: canvasOut,
@@ -149,9 +223,7 @@ $(function() {
           In: canvasIn.getContext('2d'),
           Out: canvasOut.getContext('2d'),
         },
-        Const: {
-          InitScore: -1,
-        },
+        ScaledImgSize: null,
         Counter: {
           Iteration: {
             Successful: 0,
@@ -164,6 +236,18 @@ $(function() {
             },
           },
           Score: -1,
+          Time: {
+            TimerStart: -1,
+            PauseStart: -1,
+            dur: function () {
+              return this.TimerStart > 0
+                ? Utils.currentMillis() - this.TimerStart
+                : 0;
+            }
+          },
+        },
+        PauseRequest: {
+          flag: false,
         }
     };
 
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -16,6 +16,19 @@ function UtilsConstructor() {
   this.randomRgba = function() {
     return `rgba(${Utils.randomByte()},${Utils.randomByte()},${Utils.randomByte()},${Math.random()})`
   };
+
+  this.currentMillis = function () {
+    return new Date().getTime();
+  };
+
+  this.millisToTimeStr = function(millis) {
+    let r = Math.round;
+    let p = x => String(x).padStart(2, '0');
+    let seconds = r(millis / 1000) % 60;
+    let minutes = r((millis / 1000) / 60) % 60;
+    let hours = r(((millis / 1000) / 60) / 60);
+    return `${hours ? `${p(hours)}:` : ''}${p(minutes)}:${p(seconds)}`;
+  }
 }
 
 const Utils = Object.freeze(new UtilsConstructor());
